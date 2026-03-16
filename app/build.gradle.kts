1b3961348f7e80047f5e3790913e4ebb1bb30050
dogi <dogi@users.noreply.github.com>
2026-03-16 15:49:45 -0400

    all: new versioning for app stores (#32)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8 Beta"
+        versionCode = 1
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
