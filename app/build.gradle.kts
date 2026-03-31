f56488b9e2ced998e17ddb023e357025d7e59217
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-03-31 14:15:41 -0600

    Bump app version to 0.0.44
    Increment versionCode to 44 and versionName to 0.0.44 in build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 43
-        versionName = "0.0.43"
+        versionCode = 44
+        versionName = "0.0.44"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
