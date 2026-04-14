2f4b8a97dd8b6d12c2599932fe5bda236e90d7c5
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-04-14 08:35:05 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,13 +20,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-<<<<<<< fix-cleartext-sharedprefs-vulnerability-15324476779489030893
         versionCode = 122
         versionName = "0.1.22"
-=======
-        versionCode = 121
-        versionName = "0.1.21"
->>>>>>> main
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
