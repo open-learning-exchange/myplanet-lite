fa03fb81dc33b53ef2f67271b49a5e912f4ef6fc
dogi <dogi@users.noreply.github.com>
2026-04-20 15:23:31 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 187
-        versionName = "0.1.87"
+        versionCode = 189
+        versionName = "0.1.89"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
