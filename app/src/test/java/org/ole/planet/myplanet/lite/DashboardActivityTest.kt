c85ea1305cc64a1ff81efd84ec2cd1e419d53141
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-04-23 14:11:03 -0600

    remove unused.

--- app/src/test/java/org/ole/planet/myplanet/lite/DashboardActivityTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/lite/DashboardActivityTest.kt
@@ -11,12 +11,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
