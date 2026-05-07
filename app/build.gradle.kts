3778d86bea950c6ce0bcba2d4ee068678f51a587
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-05-07 13:24:48 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 307
-        versionName = "0.3.7"
+        versionCode = 312
+        versionName = "0.3.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
