a9e25655876031e41bb88cbf37389896dd23e87e
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-06-23 15:40:06 -0600

    Show Snackbar When Exam Is Required to Continue (#1036)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "org.ole.planet.myplanet.lite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 441
-        versionName = "0.4.41"
+        versionCode = 442
+        versionName = "0.4.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "PLANET_BASE_URL", "\"http://10.82.1.30/\"")
