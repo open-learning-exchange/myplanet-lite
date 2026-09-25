42d1d26336a66a522bd8a211d6848e262c060f53
Plataformas Informáticas <wlopez@plataformasinformaticas.com>
2026-09-25 15:25:50 -0600

    fix roboelectric

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,8 +72,13 @@ tasks.withType<Test>().configureEach {
             "--add-opens=java.base/java.io=ALL-UNNAMED",
             "--add-opens=java.base/java.net=ALL-UNNAMED",
             "--add-opens=java.base/java.nio=ALL-UNNAMED",
+            "--add-opens=java.base/java.security=ALL-UNNAMED",
+            "--add-opens=java.base/java.text=ALL-UNNAMED",
             "--add-opens=java.base/java.util.concurrent=ALL-UNNAMED",
+            "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
             "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",
+            "--add-opens=java.desktop/java.awt.font=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
         )
     }
     systemProperty("robolectric.logging", "none")
